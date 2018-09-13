924e22adca1ff159872a5e8b29f384cb003e4a6a
Bill <bill.kurtson@gmail.com>
2018-09-13 10:56:14 +0200

    - add instance of Axios to use for Blog (#6)
    - set default URL for Axios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com/';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+
+axios.interceptors.request.use(requestConfig => {
+        console.log(requestConfig);
+
+        return requestConfig;
+    }, error => {
+        console.log('from index.js', error);
+
+        return Promise.reject(error);
+    });
+
+axios.interceptors.response.use(responseConfig => {
+        console.log(responseConfig);
+
+        return responseConfig;
+    }, error => {
+        console.log('from index.js', error);
+
+        return Promise.reject(error);
+    });
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
