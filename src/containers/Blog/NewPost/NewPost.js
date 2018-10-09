32ae87bfcebfaff2e4da96b4e0cdd1c92911e0e9
Bill Kurtson <bill.kurtson@gmail.com>
2018-10-09 15:55:35 +0200

    Routes: - redirect after posting new post - show loader when posting

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import './NewPost.css';
 
@@ -7,7 +8,9 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        posting: false,
+        submitted: false
     }
 
     componentDidMount() {
@@ -20,15 +23,21 @@ class NewPost extends Component {
             body: this.state.content,
             author: this.state.author
         };
+
+        this.setState({ posting: true });
+
         axios.post('posts', data)
             .then(response => {
                 console.log(data);
+                this.setState({ submitted: true, posting: false });
             });
     }
 
     render() {
-        return (
+        let redirect = this.state.submitted ? <Redirect to="/posts" /> : null;
+        let content = (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })} />
@@ -36,10 +45,28 @@ class NewPost extends Component {
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({ content: event.target.value })} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({ author: event.target.value })}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                    <option value="Jean Paul">Jean Paul</option>
+                    <option value="Kei">Kei</option>
+                    <option value="Nami">Nami</option>
+                    <option value="Fabian">Fabian</option>
                 </select>
                 <button onClick={this.postDataHandler}>Add Post</button>
+            </div>
+        );
+
+        if (this.state.posting) {
+            content = (
+                <div style={{ textAlign: 'center' }}>
+                    <div className="lds-dual-ring"></div>
+                    <div>
+                        Posting...
+                    </div>
+                </div>);
+        }
+
+        return (
+            <div>
+                {content}
             </div>
         );
     }
